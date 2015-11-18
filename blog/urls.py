80d420b0fe0295c3610be69150dccff171496dfd
abhidnya <akawli@mail.bradley.edu>
2015-11-18 00:11:09 -0600

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      chase.mcnealy
+#
+# Created:     17/11/2015
+# Copyright:   (c) chase.mcnealy 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
