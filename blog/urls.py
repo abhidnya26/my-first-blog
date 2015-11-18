203c2a349c2c30e99e7a6a370ff3510c44a703dd
abhidnya <akawli@mail.bradley.edu>
2015-11-18 10:48:52 -0600

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,9 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      chase.mcnealy
-#
-# Created:     17/11/2015
-# Copyright:   (c) chase.mcnealy 2015
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit')
 ]
